590e9c661d067cd22040788dd19f6fec43790fd1
Mirko Primorac <mirko.primorac@inet.hr>
2017-06-17 22:49:01 +0200

    programcic za generiranje neponavljajucih loto brojeva

---
+++ README.md
@@ -0,0 +1,1 @@
+# DZ_10_loto_brojevi
---
+++ loto_brojevi_2.py
@@ -0,0 +1,20 @@
+def generiranje(broj, lista):
+	'''Funkcija za generiranje proizvoljno duge liste neponavljajucih slucajnih brojeva.'''
+	from random import shuffle, seed #Ucitavanje funkcija shuffle i seed iz modula random.
+	seed() #Postavljanje generatora pseudo-slucajnih brojeva na slucajno :-)
+	niz = [i for i in range(1, lista + 1)] #Generiranje pocetne liste brojeva.
+	#print(niz) #provjera za debugiranje
+	shuffle(niz) #Permutacija liste brojeva.
+	#print(niz) #provjera za debugiranje
+	del(niz[broj:]) #Brisanje viska permutirane liste.
+	#print(niz) #provjera za debugiranje
+	return niz #Izlazna vrijednost funkcije.
+
+def main():
+	'''Glavna funkcija programa za generiranje proizvoljno duge liste neponavljajucih slucajnih brojeva.'''
+	broj = int(input('Upišite koliko brojeva želite generirati: '))
+	lista = int(input('Upišite broj do kojega želite generiranje liste: '))
+	print(generiranje(broj, lista)) #Poziv funkcije "generiranje" sa odgovarajucim vrijednostima.
+
+if __name__ == '__main__':
+	main()
